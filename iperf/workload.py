e496155f6efa42cfae7b3afb0e4a373e9e619842
Maik Srba <msrba@scotty.mikelangelo.gwdg.de>
2017-12-13 11:07:45 +0100

    use iperf resource endpoint

--- iperf/workload.py
+++ iperf/workload.py
@@ -1,24 +1,21 @@
 import logging
 
+import paramiko
+from scotty import utils
+
 logger = logging.getLogger(__name__)
 
 
 class Benchmark(object):
     def __init__(self, context):
         self.context = context
-        self.iperf_endpoint = {
-            'iperf-server': {
-            'ip': '10.254.1.198',
-            'user': 'scotty',
-            'password': '1PV4JSP5'
-          },
-          'iperf-client': {
-            'ip': '141.5.113.248'
-            'user': 'scotty',
-            'password': '1PV4JSP5'
-          }
-        }
-
+        workload = context.v1.workload
+        exp_helper = utils.ExperimentHelper(context)
+        resource_name = workload.resources['iperf']
+        logger.debug("Resource name: {}".format(resource_name))
+        resource = exp_helper.get_resource(resource_name)
+        self.iperf_endpoint = resource.endpoint
+        
     def run(self):
         logger.info('Run iperf client')
         self._execute_remote_iperf_client(
@@ -29,11 +26,10 @@ class Benchmark(object):
         )
 
     def _execute_remote_iperf_client(self, iperf_client_ip, iperf_server_ip, iperf_user, iperf_password):
-        with paramiko.SSHClient() as ssh
+        with paramiko.SSHClient() as ssh:
             ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            ssh.connect(iperf_client_ip, username=iperf_scotty_user, password=iperf_scotty_password)
+            ssh.connect(iperf_client_ip, username=iperf_user, password=iperf_password)
             cmd = "iperf -c {}".format(iperf_server_ip)
             stdin, stdout, stderr = ssh.exec_command(cmd)
             out = stdout.read()
-        logger.info("IPerf:")
-        logger.info(out)
+        logger.info("IPerf:\r\n{}".format(out))
--- workload_gen.py
+++ workload_gen.py
@@ -1,13 +1,22 @@
 import logging
 
-import iperf.workload.Benchmark
+import iperf.workload
 
 logger = logging.getLogger(__name__)
 
 def run(context):
     logger.info('Run iperf workload with scotty')
     iperf_benchmark = iperf.workload.Benchmark(context)
+    reduce_logging()
     iperf_benchmark.run()
 
 def clean(context):
     pass
+
+def reduce_logging():
+    reduce_loggers = {
+        'paramiko',
+        'paramiko.transport'
+    }
+    for logger in reduce_loggers:
+        logging.getLogger(logger).setLevel(logging.WARNING)
